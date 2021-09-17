b53dc828a1a0d1495f3955d8dbc043c0562d6f66
Malak <david.tejeda.santos@gmail.com>
2021-09-17 14:24:02 -0500

    he anadido la propiedad de una imagen

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -10,19 +10,44 @@ class CardPage extends StatelessWidget {
       body: ListView(
         padding: //EdgeInsets.all(20),
             EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-        children: [
-          _cardTipo1(),
-        ],
+        children: [_cardTipo1(), _cardTipo2()],
       ),
     );
   }
 
   Widget _cardTipo1() {
     return Card(
-      child: ListTile(
-        leading: Icon(Icons.ac_unit_rounded),
-        title: Text('Mi primera tarjeta'),
-      ),
-    );
+        child: Column(
+      children: <Widget>[
+        ListTile(
+          title: Text('Hola mundo'),
+          leading: Icon(Icons.ac_unit_sharp),
+          subtitle:
+              Text('Esta es una descripción que le he asignado a esta tarjeta'),
+        ),
+        Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+          TextButton(onPressed: () => {}, child: Text('Cancelar')),
+          TextButton(onPressed: () => {}, child: Text('OK'))
+        ]),
+      ],
+    ));
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+        child: Column(children: <Widget>[
+      FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+          height: 400.0,
+          fit: BoxFit.cover,
+          image: NetworkImage(
+              'https://images.hdqwalls.com/wallpapers/nature-landscape-mountains-5k-ev.jpg')),
+      //Image(image: NetworkImage('https://images.hdqwalls.com/wallpapers/nature-landscape-mountains-5k-ev.jpg')),
+      Container(
+        padding: EdgeInsets.all(20.0),
+        child: Text('no tengo que poner'),
+      )
+    ]));
   }
 }
